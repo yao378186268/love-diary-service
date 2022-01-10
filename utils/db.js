1e4173b4b08b5e058f925699d713a87bd589cc69
姚成成 <378186268@qq.com>
2022-01-10 10:37:47 +0800

    数据库配置

--- utils/db.js
+++ utils/db.js
@@ -6,20 +6,14 @@ let mysql = require("mysql")
  * @returns {any}
  */
 function getConnection() {
-  // let connect = mysql.createConnection({
-  //     host: '这里是数据库IP地址，127.0.0.1',
-  //     user:'这里是数据库的用户名，默认root',
-  //     port: '这里是数据库的端口号，默认3306',
-  //     password: '这里是数据库密码',
-  //     database: "这里是数据库名称"
-  // })
   let connect = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    port: "3306",
-    password: "210130",
-    database: "love_diary",
+      host: '这里是数据库IP地址，127.0.0.1',
+      user:'这里是数据库的用户名，默认root',
+      port: '这里是数据库的端口号，默认3306',
+      password: '这里是数据库密码',
+      database: "这里是数据库名称"
   })
+  
   return connect
 }
 
