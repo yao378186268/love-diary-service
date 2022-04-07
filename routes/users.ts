45c8e677331c81f1aa22195a3371f9504b8db072
yaochengcheng <378186268@qq.com>
2022-04-07 19:09:26 +0800

    picture

--- routes/users.ts
+++ routes/users.ts
@@ -3,12 +3,15 @@ var router = express.Router();
 
 // 登录接口
 let { login } = require('./login/login');
-
 router.post('/login', login);
 
 // 首页菜单接口
 let { menu } = require('./index/menu');
-
 router.get('/menu', menu);
+
+// 图片接口
+let { picture } = require('./picture/picture')
+router.get('/picture', picture)
+
 
 module.exports = router;
